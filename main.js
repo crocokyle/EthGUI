8af24fa390144ec6be85121491dd0d89e60eb919
crocokyle <me@kyle-dixon.com>
2021-08-17 20:13:02 -1000

    Added real-time charts

--- main.js
+++ main.js
@@ -12,8 +12,12 @@ let mainWindow;
 let settingsWindow;
 var miner;
 var running;
-var refreshRate = 2000;
+var refreshRate = 5000;
 var socketClient;
+var hashrateDeque = [0];
+var hrChart;
+var tempChart;
+var shareChart;
 
 // Listen for app to be ready
 app.on('ready', function() {
@@ -46,6 +50,7 @@ app.on('ready', function() {
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // Insert menu - comment out for dev menu
     Menu.setApplicationMenu(mainMenu);
+    
 });
 
 // Menu Template
@@ -149,6 +154,9 @@ function startMining(state, address, tempLimit, pool){
     // Start/stop mining
     if (state === "Start Mining!") {
         // Update UI Elements
+        // Init charts
+        initCharts()
+
         button.textContent = 'Stop Mining';
         addressBox.disabled = true;
         poolBox.disabled = true;
@@ -184,29 +192,171 @@ function startMining(state, address, tempLimit, pool){
     }
 }
 
+function initCharts() {
+    var Chart = require('chart.js');
+    if (hrChart) {hrChart.destroy()}
+    hrChart = new Chart(document.getElementById("hashrate-chart"), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{ 
+            data: [0],
+            label: "Total Hashrate",
+            borderColor: "#3e95cd",
+            fill: true,
+            tension: 0.4
+        }
+        ]
+    },
+    options: {
+        radius: 0,
+        scales: {
+            y: {
+                min: 0,
+            }
+        },
+        title: {
+        display: true,
+        text: 'World population per region (in millions)'
+        }
+    }
+    });
+    if (tempChart) {tempChart.destroy()}
+    tempChart = new Chart(document.getElementById("temp-chart"), {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{ 
+                data: [],
+                label: "GPU Temp",
+                borderColor: "#eb4034",
+                fill: true,
+                tension: 0.4
+            },
+            { 
+                data: [],
+                label: "GPU Temp Limit",
+                borderDash: [5, 5],
+                borderColor: "#db5f00",
+                fill: true
+            }
+            ]
+        },
+        options: {
+            // scales: {
+            //     y: {
+            //         min: 0,
+            //     }
+            // },
+            radius: 0,
+            title: {
+            display: true,
+            text: 'World population per region (in millions)'
+            }
+        }
+        });
+    if (shareChart) {shareChart.destroy()}
+    shareChart = new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+        labels: ["Valid Shares", "Stale Shares", "Invalid Shares"],
+        datasets: [
+            {
+            label: "Population (millions)",
+            backgroundColor: ["#3cba9f", "#fcba03","#fc0303"],
+            data: [541,31,3]
+            }
+        ]
+        },
+        options: {
+        title: {
+            display: true,
+            text: 'Shares submitted to the pool.'
+        }
+        }
+    });
+}
+
+
 function apiCall() {
     if (running) {
         setTimeout(() => {
-            request = '{"id":1,"jsonrpc":"2.0","method":"miner_getstatdetail"}'
             
             // Send the request
+            request = '{"id":1,"jsonrpc":"2.0","method":"miner_getstatdetail"}'
             socketClient = net.connect({host:'localhost', port:8888},  () => {              
                 socketClient.write(request + '\r\n');
             });
         
             // Listen for the response
+            var response;
             socketClient.on('data', (data) => {
-                var response = JSON.parse(data).result;
-                console.log(response);
+                response = JSON.parse(data).result;
+                updateUI(response);
                 socketClient.end();
             });
 
             // On disconnect
             socketClient.on('end', () => {
-                console.log('disconnected from server');
+                //console.log('Disconnected from API');
+            });
+
+            // On error
+            socketClient.on("error", (err) => {
+                console.log("Caught flash policy server socket error: ")
+                console.log(err.stack)
             });
 
             apiCall();
         }, refreshRate)
     }   
+}
+
+function updateLineChart(chart, data, maxlength) {
+    // Add hashrate to our deque
+    chart.data.labels.push(getTime())
+    var i = 0;
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data[i]);
+        i++;
+    });
+    // Remove old hashrates
+    if (chart.data.datasets[0].data.length > maxlength) {
+        chart.data.datasets[0].data.shift();
+        chart.data.labels.shift();
+    }
+    chart.update()
+}
+
+function updateUI(data) {
+    var hashrate = parseInt(data.mining.hashrate);
+    var gpuTemp = data.devices[0].hardware.sensors[0]
+    var memTemp = data.devices[0].hardware.sensors[3]
+    var tempLimit = document.getElementById('temp-limit').value;
+
+    console.log('Hashrate: ' + hashrate)
+    if (hashrate) {
+        document.getElementById('hashrate').textContent = hashrate;
+        updateLineChart(hrChart, [hashrate], 300);
+    }
+    if (gpuTemp) {
+
+        document.getElementById('gpu-temp').textContent = gpuTemp;
+        updateLineChart(tempChart, [gpuTemp, tempLimit], 300)
+        console.log(data)
+    }
+}
+
+
+function getTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+
+    return strTime;
 }
